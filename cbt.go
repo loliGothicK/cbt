b8c2c51aa145c36028e86bcfe4ed6f984ebe2c63
Yuki Wada <loligothick@gmail.com>
2017-09-21 11:03:42 +0900

    version 0.0.1

--- cbt.go
+++ cbt.go
@@ -1,59 +1,187 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-  "os"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+	"time"
 
-  "github.com/urfave/cli"
+	"github.com/LoliGothick/cbt/internal/wandbox"
+	"github.com/urfave/cli"
 )
 
 func main() {
-  app := cli.NewApp()
-  app.Name = "cbt"
-	app.Usage = "Cranberries unit test Build Tool"
+	app := cli.NewApp()
+	app.Name = "cbt (Cranberries Build Tool)"
+	app.Usage = "C++ Build Tool"
 	app.Version = "0.0.1"
 
-		app.Commands = []cli.Command{
-			// command config
-			{
-					Name:    "build",
-					Aliases: []string{"b"},
-					Usage:   "hello world を表示します",
-					Action:  Build,
+	app.Commands = []cli.Command{
+		// command config
+		{
+			Name:    "wandbox",
+			Aliases: []string{"wb"},
+			Usage:   "Send codes to wandbox.org",
+			Action:  Wandbox,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "compiler, x",
+					Value: "gcc-head",
+				},
+				cli.StringFlag{
+					Name:  "std",
+					Value: "c++14",
+				},
+				cli.StringFlag{
+					Name:  "stdin,in",
+					Value: "",
+				},
+				cli.StringFlag{
+					Name:  "option, o",
+					Value: "warnning",
+				},
+				cli.StringFlag{
+					Name:  "compile-option, c",
+					Value: "",
+				},
+				cli.StringFlag{
+					Name:  "runtime-option, r",
+					Value: "",
+				},
+				cli.BoolFlag{
+					Name: "save, s",
+				},
 			},
+		},
 	}
 
-	app.Before = func(c *cli.Context) error {
-		fmt.Println("Build Start. plz wait...")
-		return nil
-	}
+	// app.Before = func(c *cli.Context) error {
+	// 	fmt.Println("Build Start. plz wait...")
+	// 	return nil
+	// }
 
-	app.After = func(c *cli.Context) error {
-		fmt.Println("Successfuly")
-		return nil
-	}
+	// app.After = func(c *cli.Context) error {
+	// 	fmt.Println("Successfuly!")
+	// 	return nil
+	// }
 
 	app.Action = func(c *cli.Context) error {
-    fmt.Println("Hello CLI!")
-    return nil
-  }
+		fmt.Println("(´･_･`)")
+		return nil
+	}
 
-  app.Run(os.Args)
+	app.Run(os.Args)
 }
 
-func Build(c *cli.Context) {
-	
-		// グローバルオプション
-		var isDry = c.GlobalBool("dryrun")
-		if isDry {
-			fmt.Println("this is dry-run")
+func Wandbox(c *cli.Context) {
+	// preprocessing
+
+	// prepare JSON struct
+	config := wandbox.Request{}
+	// regex for C++
+	analyzer := wandbox.Analyzer{Regex: regexp.MustCompile(`#include.*".*"|".*"/\*Option\*/`)}
+	// prepare stdin
+	stdin := ([]byte)("")
+	if c.String("in") != "" {
+		_, err := os.Stat(c.String("in"))
+		if err == nil {
+			stdin, err = ioutil.ReadFile(c.String("in"))
+			if err != nil {
+				panic(err)
+			}
 		}
-	
-		// パラメータ
-		var paramFirst = ""
-		if len(c.Args()) > 0 {
-			paramFirst = c.Args().First() // c.Args()[0] と同じ意味
-		}
-	
-		fmt.Printf("Hello world! %s\n", paramFirst)
 	}
+
+	// Let's Making JSON!
+	if len(c.Args()) < 2 { // if target is a src-file
+		code, codes := analyzer.ExpandInclude(string(c.Args().First()))
+		// JSON configure
+		config = wandbox.Request{
+			Compiler:          c.String("x"),
+			Code:              code,
+			Codes:             codes,
+			Options:           strings.Join([]string{c.String("o"), c.String("std")}, ","),
+			Stdin:             string(stdin),
+			CompilerOptionRaw: c.String("c"),
+			RuntimeOptionRaw:  c.String("r"),
+			Save:              c.Bool("s"),
+		}
+	} else { // else target is multiple src-file
+		// set target
+		target := c.Args()
+		// code analyze
+		codes := analyzer.ExpandAll(target)
+		// generate template (shell)
+		tmpl := template.Must(template.ParseFiles("bash.tmpl"))
+		bash := &wandbox.Bash{
+			c.String("c"),
+			strings.Join(target, " "),
+			strings.Join([]string{c.String("o"), c.String("std"), c.String("c")}, " "),
+			string(stdin) != "false",
+			string(stdin),
+			string(("clang-head")[0:3]) != "gcc",
+		}
+		var shell = ""
+		buf := bytes.NewBufferString(shell)
+		err := tmpl.Execute(buf, bash)
+		if err != nil {
+			panic(err)
+		}
+		// JSON configure
+		config = wandbox.Request{
+			Compiler: "bash",
+			Code:     buf.String(),
+			Codes:    codes,
+			Save:     c.Bool("s"),
+		}
+	}
+
+	// Marshal JSON
+	cppJSONBytes, err := json.Marshal(config)
+	if err != nil {
+		fmt.Println("JSON config marshal error:", err)
+		return
+	}
+	out := new(bytes.Buffer)
+	json.Indent(out, cppJSONBytes, "", "    ") // pretty
+
+	file, err := os.Create(`./config.json`)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	file.Write(([]byte)(out.String()))
+
+	// Client : Wait Time 30s
+	client := &http.Client{Timeout: time.Duration(30) * time.Second}
+	// Request : POST JSON
+	req, err := http.NewRequest("POST", "https://wandbox.org/api/compile.json?", strings.NewReader(out.String()))
+	if err != nil {
+		panic(err)
+	}
+	// Header : Content-type <- application/json
+	req.Header.Add("Content-type", "application/json")
+
+	// Send POST
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	result := new(wandbox.Result)
+	if err := json.Unmarshal(([]byte)(body), result); err != nil {
+		panic(err)
+	}
+	fmt.Println(result.ProgramMessage)
+}
---
+++ internal/wandbox/detail.go
@@ -0,0 +1,141 @@
+// Package wandbox is cbt internal package.
+// Analyze code and create JSON for wandbox API.
+package wandbox
+
+import (
+	"io/ioutil"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+// Bash struct for shell script(bash) text/template
+type Bash struct {
+	Compiler  string
+	Target    string
+	Option    string
+	StdinFlag bool
+	Stdin     string
+	Clang     bool
+}
+
+// AdditionalCode is JSON Object for WandboxRequest
+type AdditionalCode struct {
+	FileName string `json:"file"`
+	Code     string `json:"code"`
+}
+
+// Request is JSON struct
+type Request struct {
+	Compiler          string           `json:"compiler"`
+	Code              string           `json:"code"`
+	Codes             []AdditionalCode `json:"codes,omitempty"`
+	Options           string           `json:"options,omitempty"`
+	Stdin             string           `json:"stdin,omitempty"`
+	CompilerOptionRaw string           `json:"compiler-option-raw,omitempty"`
+	RuntimeOptionRaw  string           `json:"runtime-option-raw,omitempty"`
+	Save              bool             `json:"save,omitempty"`
+}
+
+// Result is JSON struct
+type Result struct {
+	Status          string `json:"status"`
+	Signal          string `json:"signal"`
+	CompilerOutput  string `json:"compiler_output"`
+	CompilerError   string `json:"compiler_error"`
+	CompilerMessage string `json:"compiler_messagestdin"`
+	ProgramOutput   string `json:"program_output"`
+	ProgramError    string `json:"program_error"`
+	ProgramMessage  string `json:"program_message"`
+	Permlink        string `json:"permlink"`
+	URL             string `json:"url"`
+}
+
+func unique(path string, m map[string]string) string {
+	for _, ok := m[path]; ok; _, ok = m[path] {
+		path = "_" + path
+	}
+	return path
+}
+
+func flat(rm [][]string) []string {
+	ret := []string{}
+	for _, s := range rm {
+		str := strings.Join(s, "")
+		ret = append(ret, str[strings.Index(str, "\"")+1:strings.LastIndex(str, "\"")])
+	}
+	return ret
+}
+
+// Analyzer is Code Analyzer.
+// Expanding Include codes.
+type Analyzer struct {
+	Regex *regexp.Regexp
+	Codes map[string]string
+}
+
+// ExpandInclude : Expand only include files(for one file compilation)
+func (analyzer *Analyzer) ExpandInclude(file string) (string, []AdditionalCode) {
+	return analyzer.expand([]string{file}, file)
+}
+
+// ExpandAll : Expand all files(for muliple file compilation)
+func (analyzer *Analyzer) ExpandAll(files []string) []AdditionalCode {
+	_, ret := analyzer.expand(files, "false")
+	return ret
+}
+
+func (analyzer *Analyzer) expand(files []string, all string) (string, []AdditionalCode) {
+	if analyzer.Codes == nil {
+		analyzer.Codes = map[string]string{}
+	}
+	init := map[string]string{}
+	for _, file := range files {
+		init[file] = file
+	}
+	ret := []AdditionalCode{}
+	object := analyzer.analyzingTo(init)
+	prog := "false"
+	if all != "false" {
+		prog = object[all]
+		delete(object, all)
+	}
+	for file, code := range object {
+		ret = append(ret, AdditionalCode{file, code})
+	}
+	return prog, ret
+}
+
+func (analyzer *Analyzer) analyzingTo(files map[string]string) map[string]string {
+	var rest = map[string]string{}
+
+	for file, rename := range files {
+		analyzer.Codes[rename] = ""
+		dir := filepath.Dir(file)
+		src, err := ioutil.ReadFile(file)
+		if err != nil {
+			panic(err)
+		}
+		matched := analyzer.Regex.FindAllStringSubmatch(string(src), -1)
+		if len(matched) == 0 {
+			analyzer.Codes[rename] = string(src)
+			continue
+		} else {
+			for _, include := range matched {
+				str := strings.Join(include, "")
+				path := str[strings.Index(str, "\"")+1 : strings.LastIndex(str, "\"")]
+				next := filepath.Join(dir, path)
+				xtRename := unique(filepath.Base(path), analyzer.Codes)
+				rest[next] = xtRename
+				src = regexp.MustCompile(regexp.QuoteMeta(path)).ReplaceAll(src, ([]byte)(xtRename))
+			}
+			analyzer.Codes[rename] = string(src)
+		}
+
+	}
+	if len(rest) != 0 {
+		return analyzer.analyzingTo(rest)
+	}
+
+	return analyzer.Codes
+}
